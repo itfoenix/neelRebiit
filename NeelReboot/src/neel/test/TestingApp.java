79085ca476d9efb743ad68cfd75ada999e761520
NARENDRA JADHAV <NARENDRA JADHAV@NARENDRA>
2018-09-01 13:34:53 +0530

    done

--- NeelReboot/src/neel/test/TestingApp.java
+++ NeelReboot/src/neel/test/TestingApp.java
@@ -11,4 +11,14 @@ package neel.test;
  */
 public class TestingApp {
 
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
 }
